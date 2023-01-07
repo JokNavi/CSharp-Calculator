b9cf96b7fc276e4e7cb81f46067c65e375931d2d
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-01-07 14:42:55 +0100

    Add project files.

---
+++ CSharp Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpCalculator;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("what's your name? ");
+        Console.Write("My name is: ");
+        string userName = AskInput("Bob");
+        Console.WriteLine(userName);
+    }
+    public static string AskInput(string defaultValue)
+    {
+        string? input = Console.ReadLine();
+        return String.IsNullOrEmpty(input) ? defaultValue : input;
+    }
+}
+
