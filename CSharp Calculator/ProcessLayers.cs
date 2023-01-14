17ae7542dd7e7df67a569e73432f2e1c018ea48b
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-01-14 16:53:15 +0100

    Added/fixed ProcessLayers.ProcessAllLayers()

--- CSharp Calculator/ProcessLayers.cs
+++ CSharp Calculator/ProcessLayers.cs
@@ -2,27 +2,29 @@
 {
     internal class ProcessLayers
     {
-        private bool reachedBottom = false;
 
         internal float ProcessAllLayers(List<object> inputList)
         {
-
-            for (int i = 0; i < inputList.Count; i++)
+            if (!inputList.OfType<List<object>>().Any()) 
             {
                 
-                if (!inputList.OfType<List<object>>().Any())
+                return ProcessLayer(inputList);
+            }
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (inputList[i] is List<object>)
                 {
-                    Console.WriteLine("Bottom reached!");
-                    Console.WriteLine(inputList);
-                    reachedBottom = true;
-                    Console.WriteLine(inputList[i]);
-                    return 0;
+                    inputList[i] = ProcessAllLayers((List<object>)inputList[i]);
                 }
-                else if (inputList[i] is List<object>)
-                {
-                    ProcessAllLayers((List<object>)inputList[i]);
-                }
-                
+                                
+            }
+            return 0;
+        }
+        internal float ProcessLayer(List<object> inputList)
+        {
+            foreach (object item in inputList)
+            {
+                Console.WriteLine(item.ToString());
             }
             return 0;
         }
