441b3409aed81d1b23575119d1c12f84095e20b7
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-01-14 14:13:23 +0100

    Started work on V2. finished CreateNumbers.cs

---
+++ CSharp Calculator/CreateNumbers.cs
@@ -0,0 +1,36 @@
+﻿namespace CSharp_Calculator
+{
+    internal class CreateNumbers
+    {
+        private List<string> finalEquation = new List<string>();
+        private List<string> inputEquation;
+        private string currentNumber = "";
+        private bool appended = false;
+        internal CreateNumbers(string inputEquationString) => inputEquation = inputEquationString.Select(c => c.ToString()).ToList();
+
+        internal List<string> CreateNumberGroups()
+        {
+            for (int i = 0; i < inputEquation.Count; i++)
+            {
+                if (StringIsNumber(inputEquation[i]) || (appended == false && (inputEquation[i] == "+" || inputEquation[i] == "-") && i != inputEquation.Count && StringIsNumber(inputEquation[i+1])) || (i != 0 && inputEquation[i] == "." && StringIsNumber(inputEquation[i - 1])))
+                {
+                    Console.WriteLine($"Appended: {inputEquation[i]}");
+                    currentNumber += inputEquation[i];
+                    appended = true;
+                }
+                else 
+                {
+                    Console.WriteLine($"Pushed: {inputEquation[i]}");
+                    if (appended) { finalEquation.Add(currentNumber); currentNumber = ""; }
+                    finalEquation.Add(inputEquation[i]);
+                    appended = false;
+                }
+            }
+            if (appended) { finalEquation.Add(currentNumber); currentNumber = ""; }
+            return finalEquation;
+        }
+
+        static private bool StringIsNumber(string i) => double.TryParse(i, out _);
+
+    }
+}
--- CSharp Calculator/Program.cs
+++ CSharp Calculator/Program.cs
@@ -1,112 +1,16 @@
-﻿using System.Xml;
-
-namespace CSharp_Calculator
+﻿namespace CSharp_Calculator
 {
-
     internal class Program
     {
         private static void Main(string[] args)
         {
-            //Calculator();
-            InputStringToTokens inputStringToTokens = new InputStringToTokens("1+1-1");
-            LayerCollection convertedEquation = inputStringToTokens.TokeniseEquation();
+            Console.Write("Please enter your equation: ");
+            string input = Console.ReadLine();
+            //validateInput
+            CreateNumbers createGroups = new CreateNumbers(input);
+            List<string> numberGroups = createGroups.CreateNumberGroups();
+            Console.WriteLine($"[{string.Join(", ", numberGroups)}]");
 
-            Calculate calc = new Calculate();
-            double output = calc.workOutLayer(convertedEquation);
-            Console.WriteLine(output);
-        }
-
-        private static void Calculator()
-        {
-            Console.Write("Please input your calculation: ");
-            string? inputEquation = Console.ReadLine();
-            if (String.IsNullOrEmpty(inputEquation)) { Console.WriteLine("Equation is empty."); return; }
-
-            InputStringToTokens inputStringToTokens = new InputStringToTokens(inputEquation);
-            LayerCollection convertedEquation = inputStringToTokens.TokeniseEquation();
-            //Console.WriteLine(string.Join(',', convertedEquation.layerContent));
-
-            SplitLayers splitLayers = new SplitLayers(convertedEquation);
-            convertedEquation = splitLayers.SplitEquation();
-            Console.WriteLine("Done");
         }
     }
-
-    /*
-    * iToken interface used to make LayerCollection be able to hold 2 types: OperatorInfo and NumberInfo.
-    * 
-    * @version: 1.0
-    * 
-    * @constructor {iToken}
-    */
-    internal interface iToken
-    {
-        public string tokenString
-        {
-            get;
-            set;
-        }
-    }
-
-    /*
-    * OperatorInfo constructor used to give Operators an object type.
-    * 
-    * @version: 1.0
-    * 
-    * @constructor {OperatorInfo}
-    */
-    internal class OperatorInfo : iToken
-    {
-        internal string _tokenString = "";
-        public string tokenString
-        {
-            get => _tokenString;
-            set => _tokenString = value;
-        }
-        internal bool isLayerSeparator => this.tokenString.Contains('(') || this.tokenString.Contains(')');
-        internal OperatorInfo(string operatorString) { this.tokenString = operatorString; }
-    }
-
-    /*
-    * LayerCollection constructor used for keeping track of a number's properties.
-    * 
-    * @version: 2.1
-    * 
-    * @constructor {NumberCollection}
-    */
-    internal class NumberInfo : iToken
-    {
-        internal string _tokenString = "";
-        public string tokenString
-        {
-            get => _tokenString;
-            set => _tokenString = value;
-        }
-        internal NumberInfo(string inputString) { this.tokenString = inputString; }
-        internal bool isFloat => this.tokenString.Contains('.');
-
-    }
-    /*
-    * LayerCollection constructor used for keeping track of nested levels
-    * 
-    * @version: 2.0
-    * 
-    * @constructor {LayerCollection}
-    */
-    internal class LayerCollection : iToken
-    {
-
-        internal string _tokenString = "";
-        public string tokenString
-        {
-            get => _tokenString;
-            set => _tokenString = value;
-        }
-
-        internal List<iToken> layerContent = new List<iToken>();
-        internal int depth;
-        internal LayerCollection(int depth) { this.depth = depth; }
-    }
-
-
 }
