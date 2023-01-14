da16bdcfe98a1cc82428134b41ce9213e48c88d7
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-01-14 14:43:42 +0100

    Added a bit more to CreateLayers.cs

--- CSharp Calculator/CreateLayers.cs
+++ CSharp Calculator/CreateLayers.cs
@@ -2,21 +2,23 @@
 {
     internal class CreateLayers
     {
-        private List<object> finalEquation = new List<object>();
+        private List<object> finalEquation;
         private List<object> currentLayer;
         internal CreateLayers(List<string> input) => currentLayer = new List<object>(input);
 
         internal List<object> CreateLayersGroups()
         {
-            int firstOpenBracketIndex = 0;
-            int lastClosedBracketIndex = 0;
-            if (!currentLayer.Contains("(")) { return finalEquation; }
+            if (!currentLayer.Contains("(")) { finalEquation = new List<object>(); return finalEquation; }
 
             for (int i = 0; i < currentLayer.Count; i++)
             {
-                firstOpenBracketIndex = currentLayer.FindIndex(a => (string)a == "(");
-                lastClosedBracketIndex = currentLayer.FindLastIndex(a => (string)a == ")");
+                int firstOpenBracketIndex = currentLayer.FindIndex(a => (string)a == "(");
+                int lastClosedBracketIndex = currentLayer.FindLastIndex(a => (string)a == ")");
+                List<object> tempList = currentLayer.GetRange(firstOpenBracketIndex, lastClosedBracketIndex - firstOpenBracketIndex + 1);
+                currentLayer.RemoveRange(firstOpenBracketIndex, lastClosedBracketIndex - firstOpenBracketIndex + 1);
+                currentLayer.Insert(firstOpenBracketIndex, tempList);
             }
+            
             return finalEquation;
         }
     }
