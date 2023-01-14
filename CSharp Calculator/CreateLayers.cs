39db44a49466a833dc95242212747f1c9d2c165e
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-01-14 14:35:50 +0100

    Started work on CreateLayers.cs

--- CSharp Calculator/CreateLayers.cs
+++ CSharp Calculator/CreateLayers.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharp_Calculator
+﻿namespace CSharp_Calculator
 {
     internal class CreateLayers
     {
+        private List<object> finalEquation = new List<object>();
+        private List<object> currentLayer;
+        internal CreateLayers(List<string> input) => currentLayer = new List<object>(input);
+
+        internal List<object> CreateLayersGroups()
+        {
+            int firstOpenBracketIndex = 0;
+            int lastClosedBracketIndex = 0;
+            if (!currentLayer.Contains("(")) { return finalEquation; }
+
+            for (int i = 0; i < currentLayer.Count; i++)
+            {
+                firstOpenBracketIndex = currentLayer.FindIndex(a => (string)a == "(");
+                lastClosedBracketIndex = currentLayer.FindLastIndex(a => (string)a == ")");
+            }
+            return finalEquation;
+        }
     }
 }
