b5e3c2a65358483e4484dcf94868fa2ac44ecacd
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-01-15 11:00:23 +0100

    Added WorkOutLayer.cs

--- CSharp Calculator/ProcessLayers.cs
+++ CSharp Calculator/ProcessLayers.cs
@@ -4,25 +4,15 @@
     {
         internal string ProcessAllLayers(List<object> inputList)
         {
-            // check if input list is empty, return default value or throw error
-            if (inputList.Count == 0)
-            {
-                return "0";
-            }
-            // iterate over all elements of the input list
             for (int i = 0; i < inputList.Count; i++)
             {
-                // check if the current element is a sublist
-                if (inputList[i] is List<object>)
+               if (inputList[i] is List<object>)
                 {
-                    // recursively process the sublist
                     inputList[i] = ProcessAllLayers((List<object>)inputList[i]);
                 }
             }
-            // check if the input list doesn't contain any more sublists 
             if (!inputList.OfType<List<object>>().Any())
             {
-                // process current list 
                 return ProcessLayer(inputList);
             }
             return "0";
